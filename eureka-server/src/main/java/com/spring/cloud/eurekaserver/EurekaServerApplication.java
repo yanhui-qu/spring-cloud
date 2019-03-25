772801cd28d9350d68eb627cac98e763a4b4067b
quyanhui <quyanhui@viewhigh.com>
2019-03-25 14:19:39 +0800

    @EnableEurekaServer

--- eureka-server/src/main/java/com/spring/cloud/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/spring/cloud/eurekaserver/EurekaServerApplication.java
@@ -3,6 +3,7 @@ package com.spring.cloud.eurekaserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
 
