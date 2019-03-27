3413f347e533a4ca13ecf6a978e44970504c2c86
quyanhui <quyanhui@viewhigh.com>
2019-03-27 18:00:37 +0800

    简单测试

--- eureka-client/src/main/java/com/spring/cloud/eurekaclient/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/spring/cloud/eurekaclient/EurekaClientApplication.java
@@ -4,11 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * 注：@SpringBootApplication注解只会扫描当前包及其子包
+ * 注解@EnableEurekaClient声明该程序为Eureka客户端
+ */
 @SpringBootApplication
 @EnableEurekaClient
 public class EurekaClientApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(EurekaClientApplication.class, args);
     }
 
---
+++ eureka-client/src/main/java/com/spring/cloud/eurekaclient/controller/HiController.java
@@ -0,0 +1,35 @@
+package com.spring.cloud.eurekaclient.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class HiController {
+    @Value("${server.port}")
+    String port;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping("/hi")
+    public String hello(@RequestParam String name){
+
+        return "name:" + name + ", port:" + port;
+    }
+
+    @GetMapping("/user-instance")
+    public void getUserInstance(){
+        List<ServiceInstance> instances = discoveryClient.getInstances("eureka-client");
+        final List<String> services = discoveryClient.getServices();
+        for (int i = 0; i < instances.size(); i++) {
+            ServiceInstance instance = instances.get(i);
+        }
+    }
+}
