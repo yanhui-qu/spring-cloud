8f44cfafc1ef3d362e936d4c5e20a3c10ecd7e3d
quyanhui <quyanhui@viewhigh.com>
2019-04-02 18:35:10 +0800

    spring-eureka-server-HIA

---
+++ spring-eureka-server-hia/src/main/java/spring/cloud/springeurekaserverhia/SpringEurekaServerHiaApplication.java
@@ -0,0 +1,22 @@
+package spring.cloud.springeurekaserverhia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 注：@SpringBootApplication注解只会扫描当前包及其子包
+ * 注解@EnableEurekaServer声明该程序为Eureka客户服务端
+ * spring-eureka-server-hia是高可用集群搭建，
+ * spring-eureka-server是单个eureka服务
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringEurekaServerHiaApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringEurekaServerHiaApplication.class, args);
+    }
+
+}
